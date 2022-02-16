841fe70d8bf194cc1e3a10ee1c55be768a416012
Mobile46 <byzlo685@gmail.com>
2022-02-16 09:46:47 +0300

    Update Deobfuscator.cs

--- de4dot.code/deobfuscators/dotNET_Reactor/v4/Deobfuscator.cs
+++ de4dot.code/deobfuscators/dotNET_Reactor/v4/Deobfuscator.cs
@@ -153,7 +153,7 @@ namespace de4dot.code.deobfuscators.dotNET_Reactor.v4 {
 		public override void Initialize(ModuleDefMD module) => base.Initialize(module);
 
 		static Regex isRandomName = new Regex(@"^[A-Z]{30,40}$");
-		static Regex isRandomNameMembers = new Regex(@"^[a-zA-Z0-9]{9,11}$");	// methods, fields, props, events
+		static Regex isRandomNameMembers = new Regex(@"^(?:[a-zA-Z0-9]{9,11}|[a-zA-Z0-9]{18,20})$");	// methods, fields, props, events
 		static Regex isRandomNameTypes = new Regex(@"^[a-zA-Z0-9]{18,20}(?:`\d+)?$");	// types, namespaces
 
 		bool CheckValidName(string name, Regex regex) {
